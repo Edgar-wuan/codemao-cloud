bee31429a3b33b7c0d6f51a7fe0fe8897ec2ed3a
Edgar_勿安 <evens_wuan@qq.com>
2026-05-16 06:05:51 +0000

    update index.py. v2.1
    Signed-off-by: Edgar_勿安 <evens_wuan@qq.com>

--- index.py
+++ index.py
@@ -2,10 +2,11 @@
 import tkinter as tk
 import up
 import down
-import tkinter.messagebox as msgbox
+import webbrowser
+#import tkinter.messagebox as msgbox
 #创建窗口
 window=tk.Tk()
-window.title('欢迎使用点猫云')#窗口的标题
+window.title('欢迎使用点猫云v2.1')#窗口的标题
 window.geometry('200x350')#窗口的大小
 
 #按钮的函数
@@ -16,14 +17,14 @@ def hit_me2():
     down.app.run()
   
 def hit_me3():
-    msgbox.showwarning(message='还在制作中……')
-    #webbrowser.open('')
+    #msgbox.showwarning(message='还在制作中……')
+    webbrowser.open('https://gitee.com/zwzhaowei/codemao-cloud')
 #按钮
 b=tk.Button(window,text='上传文件',width=20,height=5,command=hit_me1)
 b.pack()
 c=tk.Button(window,text='下载文件',width=20,height=5,command=hit_me2)
 c.pack()
-b=tk.Button(window,text='网页版软件',width=20,height=5,command=hit_me3)
+b=tk.Button(window,text='使用说明',width=20,height=5,command=hit_me3)
 b.pack()
 var=tk.StringVar()#定_')
 var.set('made by Edgar_勿安')
